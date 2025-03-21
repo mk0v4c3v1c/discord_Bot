8cad03d0eb47ba6d686b49e6b62e281c3b938989
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-03-21 20:21:24 +0100

    bot-update

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 from config import DISCORD_TOKEN, PREFIX
+import os
+
 
 #Initialazing bot
 intents = discord.Intents.default()
@@ -12,6 +14,11 @@ bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 @bot.event
 async def on_ready():
     print(f'Bot is online! We have logged in as {bot.user}')
+
+#Turn on all commands from commands folder automaticaly
+for filename in os.listdir("./commands"):
+    if filename.endswith(".py"):
+        bot.load_extension(f"commands.{filename[:-3]}")
 
 #Run bot
 bot.run(DISCORD_TOKEN)
