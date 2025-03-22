a764faa8ce3b31af316d275b76fba0e9b667092d
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-03-22 17:50:08 +0100

    on_message-update

--- events/on_message.py
+++ events/on_message.py
@@ -1,4 +1,20 @@
 from discord.ext import commands
+from database.db_handler import db
+
+class XPSystem(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+@commands.Cog.listener()
+async def on_message(self, message):
+    if message.author.bot:
+        return
+
+    db.add_user(str(message.author.id))
+    new_level = db.increment_xp(str(message.author.id))
+
+    if new_level:
+        await message.channel.send(f'{message.author.mention} acomplished **Level {new_level}**!')
 
 class MessageListener(commands.Cog):
     def __init__(self, bot):
