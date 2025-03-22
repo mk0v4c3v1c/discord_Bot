06de229720c163d6dfb0dc9780ca81e5cdce1467
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-03-22 17:45:34 +0100

    db_handler-update

--- database/db_handler.py
+++ database/db_handler.py
@@ -11,6 +11,8 @@ class DBHandler:
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY,
             discord_id TEXT UNIQUE,
+            xp INTEGER DEFAULT 0,
+            level INTEGER DEFAULT 1,
             messages_sent INTEGER DEFAULT 0
         )
         """)
@@ -24,5 +26,18 @@ class DBHandler:
     def increment_messages(self, discord_id):
         self.cursor.execute("UPDATE users SET messages_sent = messages_sent + 1 WHERE discord_id = ?", (discord_id,))
         self.conn.commit()
+
+    def increment_xp(self, discord_id, amount=10):
+        self.cursor.execute("UPDATE users SET xp = xp + ? WHERE discord_id = ?", (amount, discord_id))
+        self.conn.commit()
+
+        self.cursor.execute("SELECT xp, level FROM users WHERE discord_id = ?", (discord_id,))
+        xp, level = self.cursor.fetchone()
+        if xp >= level * 100:
+            new_level = level + 1
+            self.cursor.execute("UPDATE users SET level = ? WHERE discord_id = ?", (new_level, discord_id))
+            self.conn.commit()
+            return new_level
+        return None
 
 db = DBHandler()
