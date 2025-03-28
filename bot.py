c6289ccf581d659224dd2085e3bf370d49155ac9
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-03-28 17:10:48 +0100

    refactore-bot

--- bot.py
+++ bot.py
@@ -3,22 +3,26 @@ from discord.ext import commands
 from config import DISCORD_TOKEN, PREFIX
 import os
 
-
-#Initialazing bot
+# set intens
 intents = discord.Intents.default()
 intents.messages = True
 intents.guilds = True
 
+# initialize bot
 bot = commands.Bot(command_prefix=PREFIX, intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Bot is online! We have logged in as {bot.user}')
+    print(f'Bot is online! Logged in as {bot.user}')
 
-#Turn on all commands from commands folder automaticaly
-for filename in os.listdir("./commands"):
-    if filename.endswith(".py"):
-        bot.load_extension(f"commands.{filename[:-3]}")
+# func loading bot
+def load_extensions(bot):
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            try:
+                bot.load_extension(f"commands.{filename[:-3]}")
+                print(f"Loaded extension: {filename}")
+            except Exception as e:
+                print(f"Failed to load extension {filename}: {e}")
 
-#Run bot
-bot.run(DISCORD_TOKEN)
+load_extensions(bot)
