8701a3fd12cc2c85a985ba2224d15ed1ca5ab811
mk0v4c3v1c <mk0v4c3v1c@gmail.com>
2025-04-03 12:15:50 +0200

    add tests for backend

---
+++ tests/test_api.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from web.backend.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
